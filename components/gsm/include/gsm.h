ea6a8dc97e5094bb3267bfa3157918b6fd823455
awe <ahm.wildan16@gmail.com>
2023-04-24 23:20:58 +0700

    Refactor code

--- components/gsm/include/gsm.h
+++ components/gsm/include/gsm.h
@@ -1,19 +1,34 @@
 #ifndef GSM_H
 #define GSM_H
 
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_system.h"
+#include "esp_event.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "mqtt_client.h"
 #include "string.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
 
 #define BUF_SIZE 1024
 
-#define UART_TX_PIN 17 // change to your GSM module's TX pin
-#define UART_RX_PIN 16 // change to your GSM module's RX pin
+#define UART_TX_PIN 9 // change to your GSM module's TX pin
+#define UART_RX_PIN 10 // change to your GSM module's RX pin
 #define UART_BAUD_RATE 9600
 
-static void gsm_init(void);
+void gsm_init();
+
+void log_error_if_nonzero(const char *message, int error_code);
 
 void mqtt_init();
 
